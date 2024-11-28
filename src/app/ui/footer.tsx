f0ac72369d598b96599f10ef77ce851fcec9c81a
Imanina Majeed <imanina@recovision.net>
2024-11-29 01:26:32 +0800

    Add react-icons dependency and update footer component with additional icons

--- src/app/ui/footer.tsx
+++ src/app/ui/footer.tsx
@@ -1,17 +1,17 @@
 import Link from 'next/link';
-// import { FaLinkedin, FaGithub } from "react-icons/fa";
+import { FaLinkedin, FaGithub, FaGlobe } from "react-icons/fa";
 // import { HiDocument } from 'react-icons/hi';
 
 const Footer = () => {
     return (
         <footer className="text-black row-start-3 flex gap-6 flex-wrap items-center justify-center mt-auto p-4 bg-yellow-200">
             <Link
-                className="text-black flex items-center gap-2 hover:underline hover:underline-offset-4"
-                href="https://linkedin.com/in/imaninamajeed"
+                className="text-gray-400 flex items-center gap-2 hover:underline hover:underline-offset-4"
+                href="https://github.com/imaninamajeed"
                 target="_blank"
                 rel="noopener noreferrer"
             >
-                Footer
+                <FaGithub /> GitHub
             </Link>
         </footer>
     );
