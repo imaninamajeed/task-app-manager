fa3aef078ec89a6874d4a7c507b3e7589a5279ce
Imanina Majeed <imanina@recovision.net>
2024-12-18 12:02:12 +0800

    Refactor deleteTask function to update revalidation path and remove error handling

--- src/app/lib/actions.ts
+++ src/app/lib/actions.ts
@@ -3,11 +3,6 @@ import { sql } from "@vercel/postgres";
 import { revalidatePath } from "next/cache";
 
 export async function deleteTask(id: string) {
-    try {
-        await sql`DELETE FROM tasks WHERE id = ${id}`;
-        revalidatePath("/dashboard/tasks");
-        return { message: "Deleted Task." };
-    } catch (error) {
-        return { message: "Database Error: Failed to Delete Task." };
-    }
+    await sql`DELETE FROM tasks WHERE id = ${id}`;
+    revalidatePath("/task/list");
 }
