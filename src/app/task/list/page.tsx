3e446afb4dbf19e758e1ccd82438f547a8e8b2f8
Imanina Majeed <imanina@recovision.net>
2024-12-18 12:16:22 +0800

    Refactor TaskListPage component to use props for searchParams and await its value

--- src/app/task/list/page.tsx
+++ src/app/task/list/page.tsx
@@ -11,15 +11,13 @@ export const metadata: Metadata = {
     title: "Tasks",
 };
 
-export default async function TaskListPage({
-    searchParams,
-}: {
-    searchParams?: {
+export default async function TaskListPage(props: {
+    searchParams?: Promise<{
         query?: string;
         page?: string;
-    };
+    }>;
 }) {
-
+    const searchParams = await props.searchParams;
     const query = searchParams?.query || "";
     const currentPage = Number(searchParams?.page) || 1;
 
