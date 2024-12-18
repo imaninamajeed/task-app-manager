f01891073aa3ec2c6b52d18f28b53c64d9ebb292
Imanina Majeed <imanina@recovision.net>
2024-12-18 11:40:07 +0800

    Refactor TaskListPage to integrate pagination, search functionality, and dynamic task fetching

--- src/app/task/list/page.tsx
+++ src/app/task/list/page.tsx
@@ -1,37 +1,49 @@
-import Link from 'next/link';
-import { tasks } from '../data';
+import Pagination from "@/app/ui/task/pagination";
+import Search from "@/app/ui/search";
+import Table from "@/app/ui/task/table";
+import { CreateTask } from "@/app/ui/task/buttons";
+import { TaskTableSkeleton } from "@/app/ui/skeletons";
+import { Suspense } from "react";
+import { fetchTaskPages } from '@/app/lib/data';
+import { Metadata } from "next";
 
-export default function TaskListPage() {
+export const metadata: Metadata = {
+    title: "Tasks",
+};
+
+export default async function TaskListPage({
+    searchParams,
+}: {
+    searchParams?: {
+        query?: string;
+        page?: string;
+    };
+}) {
+
+    const query = searchParams?.query || "";
+    const currentPage = Number(searchParams?.page) || 1;
+
+    const totalPages = await fetchTaskPages();
+
     return (
         <div className="container mx-auto p-4">
-            <div className="container mx-auto p-6 max-w-3xl text-center bg-gradient-to-r from-red-50 to-yellow-50 rounded-lg shadow-lg">
+            <div className="container mx-auto p-6 max-w-5xl text-center bg-gradient-to-r from-red-50 to-yellow-50 rounded-lg shadow-lg">
                 <div className="container mx-auto p-6 max-w-lg">
                     <h1 className="text-4xl font-bold text-gray-800 mb-6 text-center">Task List 📋</h1>
-                    <ul className="space-y-4">
-                        {tasks.map((task) => (
-                            <li key={task.id} className="p-4 bg-white shadow-md rounded-lg hover:shadow-lg transition-shadow">
-                                <Link href={`/task/list/${task.id}`}
-                                    className="text-xl font-semibold text-red-600 hover:underline">{task.title}
-                                </Link>
-                                <p className="text-gray-600 mt-2">{task.description}</p>
-                                <div className="mt-2">
-                                    <Link
-                                        href={`/task/edit/${task.id}`}
-                                        className="text-blue-500 hover:underline"
-                                    >
-                                        Edit
-                                    </Link>
-                                </div>
-                            </li>
-                        ))}
-                    </ul>
-                    <div className="mt-6 text-center">
-                        <Link href="/task/create"
-                            className="inline-block px-6 py-3 text-lg font-semibold text-white bg-red-600 rounded-md shadow-md hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-400">
-                            Add New Task
-                        </Link>
+                    <div className="mt-4 flex items-center justify-between gap-2 md:mt-8">
+                        <Search placeholder="Search tasks..." />
+                        <CreateTask />
                     </div>
                 </div>
+
+                <Suspense key={query + currentPage} fallback={<TaskTableSkeleton />}>
+                    <Table query={query} currentPage={currentPage} />
+                </Suspense>
+
+                <div className="mt-5 flex w-full justify-center">
+                    <Pagination totalPages={totalPages} />
+                </div>
+
             </div>
         </div>
     );
